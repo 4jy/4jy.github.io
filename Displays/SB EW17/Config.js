259a73f6bff18a50288d20b3c32b9d68a81e39f8
4JY <4JY@4JY.me>
2018-08-19 09:13:44 +1000

    added SBs

---
+++ Displays/SB EW17/Config.js
@@ -0,0 +1,6 @@
+/*
+Configurations
+Credits: Original code by Dacal. 
+*/
+
+var Clock = "12h";   // choose between "12h" or "24h".
---
+++ Displays/SB EW22/Config.js
@@ -0,0 +1,16 @@
+/* 
+Configurations
+Credits: original code by Dacal & Junesiphone, slightly modified by Evelyn
+*/
+
+var Clock = "12h";  // choose between "12h" or "24h"
+var Lang = "en";   // choose between "en", "ca", "fr", "de", "it", or "cz"
+
+
+/* No need to configure the following if you're using IS2 (default HTML) */
+var weathercode = 'HKXX0075';   // use zip code OR get weather code from www.weather.com, search for your city, code can be found in address bar.
+var celsius = true;
+var gpsswitch = false;
+var refreshrate = 10;   // update interval of weather, in minutes
+var IconSet = "Flex2";   // choose your weather icon pack here
+
