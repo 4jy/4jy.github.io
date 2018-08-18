23159e70a0acf9d3721b591f5a323a48e1ac4458
4JY <37602941+4jy@users.noreply.github.com>
2018-08-18 19:02:03 +1000

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function() {
-   document.body.style.width='100%';
-   document.body.style.height='100%';
+   //document.body.style.width='100%';
+   //document.body.style.height='100%';
 }, false);
 
 function updateClock() {
